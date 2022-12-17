b60aa9cc0a6ca865aaafe76318eb20bf29d87c11
omodauda <omodauda.dl@gmail.com>
2022-12-17 14:18:32 +0100

    font styles

---
+++ src/config/fontStyles.ts
@@ -0,0 +1,26 @@
+import { fontSz } from "~config";
+
+const fontStyle = {
+  titleBold: {
+    fontFamily: 'GeneralSans-Semibold',
+    fontSize: fontSz(18),
+  },
+  bodyBig: {
+    fontFamily: 'GeneralSans-Medium',
+    fontSize: fontSz(18),
+  },
+  bodyMedium: {
+    fontFamily: 'GeneralSans-Medium',
+    fontSize: fontSz(16),
+  },
+  bodySmall: {
+    fontFamily: 'GeneralSans-Medium',
+    fontSize: fontSz(12),
+  },
+  bodyRegular: {
+    fontFamily: 'GeneralSans-Regular',
+    fontSize: fontSz(14),
+  }
+}
+
+export default fontStyle;

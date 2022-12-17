b273b720d78bbcdea79dd042039cccc77cef72fd
omodauda <omodauda.dl@gmail.com>
2022-12-17 22:50:34 +0100

    Add font style prop

--- src/components/AvatarCard.tsx
+++ src/components/AvatarCard.tsx
@@ -4,18 +4,19 @@ import { wp } from '~config';
 import { fontStyle, colors } from '~config/styles';
 
 interface Props{
-  style: object
+  cardStyle: object
   name: string;
+  nameStyle?: object;
 }
 
-const AvatarCard: React.FC<Props> = ({ name, style }) => {
+const AvatarCard: React.FC<Props> = ({ name, cardStyle, nameStyle }) => {
   const names = name.split(' ')
   const first = names[0].split('')[0].toUpperCase();
   const second = names[1] !== undefined ? names[1].split('')[0].toUpperCase() : '';
   const initials = `${first}${second}`
   return (
-    <View style={[styles.card, style]}>
-      <Text style={[fontStyle.bodySmall, {color: colors.background}]}>{initials}</Text>
+    <View style={[styles.card, cardStyle]}>
+      <Text style={[fontStyle.bodySmall, nameStyle, {color: colors.background}]}>{initials}</Text>
     </View>
   )
 }

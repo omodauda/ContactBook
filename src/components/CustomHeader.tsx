205a771d634df7556ff9139888e9691f1b0868f5
omodauda <omodauda.dl@gmail.com>
2022-12-18 22:42:36 +0100

    Remove horizontal padding

--- src/components/CustomHeader.tsx
+++ src/components/CustomHeader.tsx
@@ -21,7 +21,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
-    paddingHorizontal: wp(16),
     paddingVertical: hp(20)
   },
   avatar: {

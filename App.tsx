f4904c10b641eb0f02ffe0a4cc843343edfcc188
omodauda <omodauda.dl@gmail.com>
2022-12-18 22:44:58 +0100

    Provide fav context to app

--- App.tsx
+++ App.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import Screens from '~navigation';
+import { FavouriteContactContext } from '~context';
 
 const App = () => {
+  const [favContact, setFavContact] = useState();
+
+  const value = useMemo(() => ({ favContact, setFavContact}), [favContact])
   return (
-    <SafeAreaProvider>
+    <FavouriteContactContext.Provider value={value}>
+      <SafeAreaProvider>
       <NavigationContainer>
         <Screens />
       </NavigationContainer>
     </SafeAreaProvider>
+    </FavouriteContactContext.Provider>
   );
 };
 
